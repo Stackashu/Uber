4a71ff2f6d8fa7c56a045480c3b6b03801778d30
Stackashu <ashu12141214@gmail.com>
2024-11-28 20:25:50 +0530

    user fiiles are created

--- Backend/app.js
+++ Backend/app.js
@@ -1,14 +1,26 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const app = express();
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+
+try {
+  connectToDb();
+} catch (error) {
+  console.error("Error connecting to database:", error);
+  process.exit(1);
+}
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("hello stack");
+});
 
-app.get("/",(req,res)=>{
-    res.send("hello stack");
-})
+app.use("/users", userRoutes);
 
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -4,5 +4,5 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 server.listen(port, ()=>{
-    console.log(`Server is runnign on part ${port}`);
+    console.log(`Server is running on part ${port}`);
 });
