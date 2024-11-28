fc63c96e8b8b1f720a41c1462d4b7fe6f5007d64
Stackashu <ashu12141214@gmail.com>
2024-11-28 21:32:03 +0530

    authorizaton done for user

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
 
@@ -16,6 +17,7 @@ try {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("hello stack");
